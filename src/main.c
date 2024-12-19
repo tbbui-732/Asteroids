40650b1b434df110fecfd6bd66aee03d7f8f3fb3
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-12-18 21:27:55 -0500

    Render the player character to the screen

--- src/main.c
+++ src/main.c
@@ -5,6 +5,34 @@
 // -- GLOBAL VARIABLES --
 const int scrWidth = 1600;
 const int scrHeight = 900;
+const int playerWidth = 100;
+const int playerHeight = playerWidth * 2;
+
+// -- player logic -- @@TODO: render triangle, keyboard input to update player position
+struct playerTriangleVertices {
+    Vector2 v1;
+    Vector2 v2;
+    Vector2 v3;
+} typedef ptv;
+
+ptv playerTriVert;
+
+Vector2 playerPosition = { scrWidth/2.0f, scrHeight/2.0f };
+
+void updatePlayerPosition() {
+    // NOTE-- recall that triangles are rendered in counter clockwise direction
+    playerTriVert.v1 = (Vector2) {playerPosition.x, playerPosition.y - playerHeight}; // Top vertex
+    playerTriVert.v2 = (Vector2) {playerPosition.x - playerWidth, playerPosition.y};  // Bottom-left vertex
+    playerTriVert.v3 = (Vector2) {playerPosition.x + playerWidth, playerPosition.y};  // Bottom-right vertex
+}
+
+
+void drawPlayer() {
+    DrawTriangle(playerTriVert.v1, playerTriVert.v2, playerTriVert.v3, VIOLET);
+}
+
+void processInput() {
+}
 
 // -- START HERE -- 
 int main(void) {
@@ -14,6 +42,8 @@ int main(void) {
     {
         BeginDrawing();
             ClearBackground(RAYWHITE);
+            updatePlayerPosition();
+            drawPlayer();
         EndDrawing();
     }
     CloseWindow();
