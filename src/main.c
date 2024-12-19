f047e9fd61757a1aa1585585db458cddfdcec405
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-12-18 21:17:22 -0500

    Render a window (START)

--- src/main.c
+++ src/main.c
@@ -1,18 +1,21 @@
+// -- INCLUDES --
 #include <stdio.h>
 #include "../include/raylib.h"
 
-int main(void) {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+// -- GLOBAL VARIABLES --
+const int scrWidth = 1600;
+const int scrHeight = 900;
 
+// -- START HERE -- 
+int main(void) {
+    InitWindow(scrWidth, scrHeight, "ASTEROIDS!");
+    SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
-
     CloseWindow();
     return 0;
 }
-
