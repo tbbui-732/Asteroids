cd37805735411e2ae1efd2241ea7bb95893dbc65
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-12-20 20:29:42 -0500

    Restructured the entire code; Wrapped Player logic into a struct

--- src/main.c
+++ src/main.c
@@ -1,85 +1,68 @@
+// -- TODOS --
+// @@TODO-- Mimick the movements of the actual asteroids game (rotation with left-right keys, glide-effect)
+// @@TODO-- deltaTime to prevent weird movements
+// @@TODO-- Rotations! !?!!
+
 // -- INCLUDES --
 #include <stdio.h>
 #include <math.h>
 #include "../include/raylib.h"
 
-// -- GLOBAL VARIABLES --
-const int scrWidth = 1600;
-const int scrHeight = 900;
-const int playerWidth = 100;
-const int playerHeight = playerWidth * 2;
-Vector2 playerSpeedVec = {10.0f, 10.0f};
-const int playerSpeed = 10.0f;
-const float pi = 3.14159265359f;
-float rotationAngle = 0.0f;
+// -- DEFINITIONS --
+#define SCRWIDTH    1600
+#define SCRHEIGHT   900
+#define SHIPWIDTH   100 // @@NOTE: the ship's width and height are arbitrary
+#define SHIPHEIGHT  200
+#ifndef PI
+    #define PI          3.14159265359f
+#endif
 
-// -- player logic -- @@TODO: render triangle, keyboard input to update player position
-struct playerTriangleVertices {
+// -- STRUCTS --
+typedef struct PlayerVertices {
     Vector2 v1;
     Vector2 v2;
     Vector2 v3;
-} typedef ptv;
+} PlayerVertices;
 
-ptv playerTriVert;
+typedef struct Player {
+    Vector2         position;
+    PlayerVertices  vertices;
+    float           speed;
+    float           acceleration;
+    float           angle;
+} Player;
 
-Vector2 playerPosition = { scrWidth/2.0f, scrHeight/2.0f };
+// -- FUNCTIONS -- 
+void Init();
+void ProcessInput();
+void Draw();
 
-// @@TODO-- Mimick the movements of the actual asteroids game (rotation with left-right keys, glide-effect)
-// @@TODO-- deltaTime to prevent weird movements
-// @@TODO-- Rotations! !?!!
-
-
-void updatePlayerPosition() {
-    // NOTE-- triangles are rendered in counter clockwise direction
-    // apply rotation to triangle
-    float radians = rotationAngle * pi / 180.0f;
-    float sinTheta = sinf(radians);
-    float cosTheta = cosf(radians);
-
-    playerTriVert.v1 = (Vector2) { // top 
-        playerPosition.x + sinTheta * playerHeight, 
-        playerPosition.y - cosTheta * playerHeight
-    };
-    playerTriVert.v2 = (Vector2) { // bottom left
-        playerPosition.x - cosTheta * playerWidth,
-        playerPosition.y - sinTheta * playerWidth 
-    };
-    playerTriVert.v3 = (Vector2) { // bottom right
-        playerPosition.x + cosTheta * playerWidth,
-        playerPosition.y + sinTheta * playerWidth 
-    };
-}
-
-void processInput() {
-    if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))
-        rotationAngle -= 2.0f;
-    if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))    
-        rotationAngle += 2.0f;
-
-    if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))
-        playerPosition.y -= playerSpeedVec;
-    else
-
-    
-    playerSpeedVec.x = sin(rotationAngle * pi / 180.0f) * playerSpeed;
-    playerSpeedVec.y = cos(rotationAngle * pi / 180.0f) * playerSpeed;
-
-
-    updatePlayerPosition();
-}
-
-// -- START HERE -- 
+// -- DRAW -- 
 int main(void) {
-    InitWindow(scrWidth, scrHeight, "ASTEROIDS!");
-    SetTargetFPS(60);
+    Init();
+
     while (!WindowShouldClose()) {
-        processInput();
-        BeginDrawing();
-            ClearBackground(RAYWHITE);
-            updatePlayerPosition();
-            DrawTriangle(playerTriVert.v1, playerTriVert.v2, playerTriVert.v3, VIOLET);
-        EndDrawing();
+        ProcessInput();
+        Draw();
     }
+
     CloseWindow();
     return 0;
+}
+
+
+// -- FUNCTION IMPLEMENTATION --
+void Init() {
+    InitWindow(SCRWIDTH, SCRHEIGHT, "ASTEROIDS");
+    SetTargetFPS(60);
+}
+
+void ProcessInput() {
+
+}
+
+void Draw() {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    EndDrawing();
 }
